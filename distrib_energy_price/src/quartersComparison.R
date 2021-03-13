6bf5d3c90034e52407e86d13ef6bc39f6ae850ff
Juan Anguita <juan.anguita@masmovil.com>
2021-03-13 11:48:59 +0000

    New file for quarters comparison

---
+++ distrib_energy_price/src/quartersComparison.R
@@ -0,0 +1,238 @@
+# Clean the Global Environment
+rm(list=ls())
+
+# Open file "vendas"
+library(readxl)
+vendas <- read_excel("GENGADI2021.xlsx", sheet = "vendas")
+attach(vendas) # the first line includes the names of the variables
+head(vendas, 6) # to see the first 6 lines
+
+# Correlation Analysis: Análise de Correlação
+plot(sales ~ adverting) # Scatter diagram: Diagrama de dispersão
+abline(lm(sales ~ adverting), col="blue", lty=1, lwd=4)
+abline(h=mean(sales), col="red", lty=1, lwd=2)
+cor(sales, adverting) # Simple Correlation Coefficient: Coeficiente de correlação simples
+
+# Computing by "hand": Cálculo manual do coeficiente de correlação simples
+r <- cov(sales, adverting)/(sd(sales)*sd(adverting))
+r
+(r <- cov(sales, adverting)/(sd(sales)*sd(adverting)))
+cor.test(sales, adverting, method="pearson") # Significance test: Teste de significância
+cor.test(sales, adverting)
+(n <- length(sales))
+(test <- r*sqrt(n-2)/sqrt(1-r^2))
+pvalue <- pt(test, n-2, lower.tail = FALSE)*2 # probability on the right: calcula a probabilidade à direita
+pvalue
+
+cor.test(sales, adverting, method="spearman") # Ordinal variables: Variáveis ordinais
+
+# Open file "vendas"
+library(readxl)
+salarios <- read_excel("GENGADI2021.xlsx", sheet = "salariosR")
+# Salaries: 1-Portugal, 2-Greece, 3-Spain
+attach(salarios) # the first line includes the names of the variables
+head(salarios, 6) # to see the first 6 lines
+
+# Histogram
+hist(salary)
+library(moments)
+skewness(salary) # 0 - no caso da normal
+kurtosis(salary) # 3 - no caso da normal
+
+# Quantiles / Percentiles Salary
+quantile(salary, c(0.01, 0.05, 0.95, 0.99))
+
+# Testing the normality of salaries
+library("nortest")
+lillie.test(salary) # Kolmogorov-Smirnov with Lilliefors correction # amostras de dimensão intermédia (30-50)
+ad.test(salary) # Anderson-Darling
+cvm.test(salary) # Cramer-von Mises
+pearson.test(salary) # Pearson Chi-Square
+sf.test(salary) # Shapiro-Francia # pequenas amostras <30
+library(tseries)
+jarque.bera.test(salary) # Jarque-Bera test (package "tseries") # Grandes amostras
+jarque.test(salary) # Jarque-Bera test (package "moments")
+
+# Teste à média do salário em Portugal = 1200; Testing the nullity of returns' mean
+head(salarios)
+portugal <- subset(salarios, country=1)
+greece <- subset(salarios, country=1)
+spain <- subset(salarios, country=1)
+
+t.test(portugal$salary, mu=1200) # H0: média na populção =1200
+
+# Comparação das variâncias: Comparing variances
+portgre <- subset(salarios, country<3)
+tapply(portgre$salary, portgre$country, var)
+
+var.test(portgre$salary ~ portgre$country) # F test
+bartlett.test(portgre$salary ~ portgre$country) # Bartlett test
+
+# Test to compare the means (Portugal and Greece)
+tapply(portgre$salary, portgre$country, mean)
+# Não esquecer que antes de avançar com o teste seguinte temos de comparar as 
+# variâncas. Se não rejeitar a =, var.equal=TRUE
+t.test(portgre$salary ~ portgre$country, paired = FALSE, var.equal=FALSE, conf.level = 0.95)
+
+# Test to compare the means (all the countries)
+tapply(salary, country, mean)
+
+tapply(salary, country, lillie.test)
+
+bartlett.test(salary ~ country)
+
+fitanova1 <- aov(salary ~ country, data=salarios) # ANOVA test
+summary(fitanova1)
+
+kruskal.test(salary ~ country, data=salarios) # Kruskal-Wallis test
+
+
+## Regression Analysis: Regression Analysis
+# Estimate the linear regression model
+
+# Install package: lmtest
+library(readxl)
+vendas <- read_excel("MEX2021.xlsx", sheet = "vendas")
+attach(vendas) # the first line includes the names of the variables
+
+library(lmtest)
+reg1 <- lm(sales ~ adverting, data = salarios)
+summary(reg1) # para aceder ao conteúdo do objeto reg1
+
+# Estimates of the coefficients
+res1 <- reg1$residuals
+plot(res1)
+reg1$coefficients
+reg1$coefficients[1]
+reg1$coefficients[2]
+
+# OLS residuals
+residuals(reg1)
+sum(residuals(reg1))
+mean(residuals(reg1))
+
+# Predicted values
+yhat <- predict(reg1)
+sum(yhat)
+sum(sales)
+plot(sales ~ yhat)
+mean(sales)
+mean(yhat)
+
+# R-Squared and adjusted R-Squared
+summary(reg1)$r.squared
+summary(reg1)$adj.r.squared
+
+# TSS
+TSS <- sum((sales-mean(sales))^2) # Variação total em torno da média
+ESS <- sum((sales-mean(sales))^2) # Variação explicada
+e <- sales-yhat # e_i = Y_i - Y^_i ## Cálculo dos resíduos
+e[1]
+e
+RSS <- sum(e^2)
+RSS
+RSS <- sum((sales-yhat)^2) # Residual Sum of Squares: Variação não explicada
+R21 <- ESS/TSS # Coeficiente de determinação, R^2
+R21
+(R21 <- ESS/TSS)
+R22 <- 1- RSS/TSS # Coeficiente de determinação, R^2
+R22
+(R22 <- 1- RSS/TSS)
+
+# F-test
+summary(reg1)$fstatistic
+summary(reg1)[10]
+summary(reg1)[4]
+
+# t-tests
+coef(summary(reg1))[, "t value"]
+
+# Previsão das vendas se  advertising = 500
+salesprev <- reg1$coefficients[1]+reg1$coefficients[2]*500
+salesprev
+
+yhat <- reg1$coefficients[1] + reg1$coefficients[2]*adverting
+yhat
+
+# Estimar o modelo de regressão linear múltipla
+head(vendas)
+varexp <- cbind(adverting, Q1, Q2, Q3)
+reg2 <- lm(sales ~ adverting + Q1 + Q2 + Q3, data = vendas)
+summary(reg2)
+reg21 <- lm(sales ~ varexp, data = vendas)
+summary(reg21) # para aceder ao conteúdo do objeto reg21
+
+# Estimate the quadratic function between sales and adverting
+adverting2 <- adverting^2
+reg3 <- lm(sales ~ adverting + adverting2, data = vendas)
+summary(reg3)
+
+# Estimate lin-lin
+regC31 <- lm(sales ~ adverting, data = vendas)
+summary(regC31)
+
+# Estimate log-lin
+regC32 <- lm(log(sales) ~ adverting, data = vendas)
+summary(regC32)
+
+# Estimate lin-log
+regC33 <- lm(sales ~ log(adverting), data = vendas)
+summary(regC33)
+
+# Estimate log-log
+regC34 <- lm(log(sales) ~ log(adverting), data = vendas)
+summary(regC34)
+
+# Comparing R-squared from different models
+# R2 from regC31
+R2C31 <- summary(regC31)$r.squared
+AR2C31 <- summary(regC31)$adj.r.squared
+
+# R2 from regC32
+# Na regC32 LOG(Y) = X2 (então a var dependente é o LOG(Y))
+predict(regC32) # ^log(Y) não ^Y
+yhat2 <- exp(predict(regC32))
+R2C32 <- cor(sales, yhat2)^2
+AR2C32 <- 1-(1-R2C32)*(60-1)/(60-2)
+
+# R2 from regC33
+R2C33 <- summary(regC33)$r.squared
+AR2C33 <- summary(regC33)$adj.r.squared
+
+# R2 from regC34
+yhat4 <- exp(predict(regC34))
+R2C34 <- cor(sales, yhat4)^2
+AR2C34 <- 1-(1-R2C34)*(60-1)/(60-2)
+
+# R2 from reg2
+yhat2 <- predict(reg2)
+R2C35 <- cor(sales, yhat2)^2
+(AR2C35 <- 1-(1-R2C35)*(60-1)/(60-5))
+
+# package "plotly"
+library(plotly)
+
+tableR2 <- plot_ly(
+  type = 'table',
+  header = list(
+    values = c('<b>GOODNESS-OF-FIT STATISTICS</b>', '<b>regC31</b>','<b>regC32</b>','<b>regC33</b>','<b>regC34</b>','<b>regC35</b>'),
+    line = list(color = '#506784'),
+    fill = list(color = '#119DFF'),
+    align = c('left','center'),
+    font = list(color = 'white', size = 12)
+  ),
+  cells = list(
+    values = rbind(
+      c('R-Squared', 'Adjusted R-squared'),
+      c(round(R2C31, digits=4), round(AR2C31, digits=4)),
+      c(round(R2C32, digits=4), round(AR2C32, digits=4)),
+      c(round(R2C33, digits=4), round(AR2C33, digits=4)),
+      c(round(R2C34, digits=4), round(AR2C34, digits=4)),
+      c(round(R2C35, digits=4), round(AR2C35, digits=4)))),
+  line = list(color = '#506784'),
+  fill = list(color = c('#25FEFD', 'white')),
+  align = c('left', 'center'),
+  font = list(color = c('#506784'), size = 12)
+)
+tableR2
+
