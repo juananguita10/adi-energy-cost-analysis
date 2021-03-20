dcdfb9032d3479db299218f9f0744ca8a9c10d97
Juan Anguita <juan.anguita@masmovil.com>
2021-03-20 00:01:52 +0000

    Comparison finished

--- distrib_energy_price/src/distributions.R
+++ distrib_energy_price/src/distributions.R
@@ -68,28 +68,59 @@ best_dist_adv <- function(d) {
 # 2020
 d2020 <- subset(EnergyPrice, year==2020)
 bdb2020 <- best_dist_basic(d2020$price) # Normal
+jarque.bera.test(d2020$price) # Testing normality -> rejected
 bda2020 <- best_dist_adv(d2020$price) # BCCGo
 
 # 2019
 d2019 <- subset(EnergyPrice, year==2019)
 bdb2019 <- best_dist_basic(d2019$price) # Normal
+jarque.bera.test(d2019$price) # Testing normality -> rejected
 bda2019 <- best_dist_adv(d2019$price) # BCPEo
 
 # 2018
 d2018 <- subset(EnergyPrice, year==2018)
 bdb2018 <- best_dist_basic(d2018$price) # Weibull
+jarque.bera.test(d2020$price) # Testing normality -> rejected # weibull should be tested
 bda2018 <- best_dist_adv(d2018$price) # BCPEo
 
 # 2017
 d2017 <- subset(EnergyPrice, year==2017)
 bdb2017 <- best_dist_basic(d2017$price) # Normal
+jarque.bera.test(d2017$price) # Testing normality -> rejected
 bda2017 <- best_dist_adv(d2017$price) # BCPE
 
 # 2016
 d2016 <- subset(EnergyPrice, year==2016)
 bdb2016 <- best_dist_basic(d2016$price) # Normal
+jarque.bera.test(d2016$price) # Testing normality -> rejected
 bda2016 <- best_dist_adv(d2016$price) # BCPE
 
 # Every year
 bdb <- best_dist_basic(EnergyPrice$price) # Normal
+jarque.bera.test(EnergyPrice$price) # Testing normality -> rejected
 bda <- best_dist_adv(EnergyPrice$price) # BCPE
+
+#----------------------------------- (delete)Part 1 ------------------------------
+# Histogram
+hist(price)
+
+(skew <- skewness(price)) # 0 - no caso da normal
+(kurt <- kurtosis(price)) # 3 - no caso da normal
+
+# Quantiles / Percentiles Salary
+(quant <- quantile(price, c(0.01, 0.05, 0.95, 0.99)))
+
+# Testing the normality of salaries
+lillie.test(price) # Kolmogorov-Smirnov with Lilliefors correction # (30-50)
+ad.test(price) # Anderson-Darling
+cvm.test(price) # Cramer-von Mises
+pearson.test(price) # Pearson Chi-Square
+sf.test(price) # Shapiro-Francia # (<30)
+jarque.bera.test(price) # Jarque-Bera test (package "tseries") # (>50)
+jarque.test(price) # Jarque-Bera test
+# Em todos rejeitamos a normal
+
+# Getting q1 subset
+q1 <- subset(EnergyPrice, q==1)
+# Energy price = 35 in Q1 test # Testing the nullity of returns' mean
+t.test(q1, mu=35) # H0 # mean = 35 -> not rejected
--- distrib_energy_price/src/quartersComparison.R
+++ distrib_energy_price/src/quartersComparison.R
@@ -6,106 +6,58 @@ library("nortest")
 # Importing Energy data
 rm(list=ls()) # Clean the Global Environment
 setwd("C:/Projects/ISCTE/ADI/adi-energy-cost-analysis/distrib_energy_price/data")
+EnergyPrice <- read_excel("OMIE_ES_MARCA_TECNOL_1_01_01_2020_30_04_2020_js.xlsx", sheet = "yearsR")
 EnergyPrice20 <- read_excel("OMIE_ES_MARCA_TECNOL_1_01_01_2020_30_04_2020_js.xlsx", sheet = "quartersR")
-attach(EnergyPrice20)
-head(EnergyPrice20)
 
-# Function passing array of numbers to condition
-get_condition_str <- function(arr, nm) {
-  if (length(arr) == 0 | nm == "") {
-    return("")  
-  }
-  
-  arrStr <- ""
-  for (i in arr) {
-    if (typeof(i) == "character") {
-      i <- paste("'", i, "'", sep="")
-    }
-    
-    if (arrStr == "") {
-      arrStr <- paste(arrStr, "(", nm, "==", i)
-    } else {
-      arrStr <- paste(arrStr, "|", nm, "==", i)
-    }
-  }
-  arrStr <- paste(arrStr, ")")
-  
-  return(arrStr)
-}
+# ----------- Correlation Analysis 2016 vs 2017 vs 2018 vs 2019 vs 2020 --------
+# Test to compare every quarter mean
+tapply(EnergyPrice$price, EnergyPrice$year, mean) # Get
 
-# Function returning a subset of the quarters needed
-get_subset_energy <- function(qArray=c(), eArray=c()) {
-  qStr <- get_condition_str(qArray, "q")
-  eStr <- get_condition_str(eArray, "tech")
-  
-  if (qStr == "" & eStr == "" ) {
-    return(price)
-  }
-  
-  if (qStr != "" & eStr != "") {
-    cStr <- paste(qStr, eStr, sep=" & ")
-  } else if (qStr != "") {
-    cStr <- qStr
-  } else if (eStr != "") {
-    cStr <- eStr
-  }
-  print(cStr)
-  
-  return(subset(price, eval(parse(text=cStr))))
-}
+#tapply(EnergyPrice$price, EnergyPrice$year, lillie.test) # normallity test not necessary
 
-#----------------------------------- Part 1 ------------------------------
-# Histogram
-hist(price)
+bartlett.test(EnergyPrice$price ~ EnergyPrice$year) # variances test -> rejected, difference between variances is statistically significant
 
-(skew <- skewness(price)) # 0 - no caso da normal
-(kurt <- kurtosis(price)) # 3 - no caso da normal
+#fitanova1 <- aov(EnergyPrice$price ~ EnergyPrice20$year, data=EnergyPrice) # ANOVA test
+#summary(fitanova1) # ANOVA can't be used due to variances significant difference  
 
-# Quantiles / Percentiles Salary
-(quant <- quantile(price, c(0.01, 0.05, 0.95, 0.99)))
+kruskal.test(EnergyPrice$price ~ EnergyPrice$year, data=EnergyPrice) # Kruskal-Wallis test -> rejected, difference between means is statistically significant
 
-# Testing the normality of salaries
-lillie.test(price) # Kolmogorov-Smirnov with Lilliefors correction # (30-50)
-ad.test(price) # Anderson-Darling
-cvm.test(price) # Cramer-von Mises
-pearson.test(price) # Pearson Chi-Square
-sf.test(price) # Shapiro-Francia # (<30)
-jarque.bera.test(price) # Jarque-Bera test (package "tseries") # (>50)
-jarque.test(price) # Jarque-Bera test
+# Getting 2019 and 2020 subset
+y19y20 <- subset(EnergyPrice, year==2019 | year==2020)
+y19 <- subset(EnergyPrice, year==2019)
+y20 <- subset(EnergyPrice, year==2020)
 
-# Getting q1 subset
-q1 <- get_subset_energy(qArray=c(1))
-# Energy price = 35 in Q1 test # Testing the nullity of returns' mean
-t.test(q1$price, mu=35) # H0 # mean = 35 -> not rejected
+# Comparing the variances
+tapply(y19y20$price, y19y20$year, var) # Get
+var.test(y19y20$price ~ y19y20$year) # F test to test equality of variances -> not rejected, difference between both variances is not statistically significant
+bartlett.test(y19y20$price ~ y19y20$year) # Bartlett test to test equality of variances -> not rejected, difference between both variances is not statistically significant
 
-#----------------------------------- Part 2 ------------------------------
-# Getting q1 and q2 subset
-q1q2 <- get_subset_energy(qArray=c(1,2))
-# Comparing variances q1 vs q2
-tapply(q1q2$price, q1q2$q, var)
-var.test(q1q2$price ~ q1q2$q) # F test
-bartlett.test(q1q2$price ~ q1q2$q) # Bartlett test
+# Comparing the means
+tapply(y19y20$price, y19y20$year, mean) # Get
+t.test(y19y20$price ~ y19y20$year, paired=TRUE, var.equal=TRUE, conf.level = 0.95) # Test equality of means -> rejected, difference between both means is statistically significant
 
-# Test to compare the means q1 vs q2
-tapply(q1q2$price, q1q2$q, mean)
-t.test(q1q2$price ~ q1q2$q, paired = FALSE, var.equal=FALSE, conf.level = 0.95)
+# Getting 2018 and 2020 subset
+y18y20 <- subset(EnergyPrice, year==2018 | year==2020)
+y18 <- subset(EnergyPrice, year==2018)
 
-# Test to compare the means (all the quarters)
-tapply(price, q, mean)
+# Comparing the variances
+tapply(y18y20$price, y18y20$year, var) # Get
+var.test(y18y20$price ~ y18y20$year) # F test to test equality of variances -> rejected, difference between both variances is statistically significant
+bartlett.test(y18y20$price ~ y18y20$year) # Bartlett test to test equality of variances -> rejected, difference between both variances is statistically significant
 
-tapply(price, q, lillie.test)
+# Comparing the means
+tapply(y18y20$price, y18y20$year, mean) # Get
+t.test(y18y20$price ~ y18y20$year, paired=TRUE, var.equal=TRUE, conf.level = 0.95) # Test equality of means -> rejected, difference between both means is statistically significant
 
-bartlett.test(price ~ q)
+# ------------- Correlation Analysis Q1 vs Q2 vs Q3 vs Q4 ----------------------
+# Test to compare every quarter mean
+tapply(EnergyPrice20$price, EnergyPrice20$q, mean) # Get
 
-fitanova1 <- aov(price ~ q, data=EnergyPrice20) # ANOVA test
-summary(fitanova1)
+#tapply(EnergyPrice20$price, EnergyPrice20$q, lillie.test) # normallity test not necessary
 
-kruskal.test(price ~ q, data=EnergyPrice20) # Kruskal-Wallis test
+bartlett.test(EnergyPrice20$price ~ EnergyPrice20$q) # variances test -> rejected, difference between variances is statistically significant
 
+#fitanova1 <- aov(EnergyPrice20$price ~ EnergyPrice20$q, data=EnergyPrice20) # ANOVA test
+#summary(fitanova1) # ANOVA can't be used due to variances significant difference  
 
-
-
-
-# Example getting tech's subsets
-allBG <- get_subset_energy(eArray=c("BG"))
-q1BG <- get_subset_energy(qArray=c(1), eArray=c("BG"))
+kruskal.test(EnergyPrice20$price ~ EnergyPrice20$q, data=EnergyPrice20) # Kruskal-Wallis test -> rejected, difference between means is statistically significant
